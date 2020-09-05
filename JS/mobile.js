f8891164048fb7da4f65c3494a1f83fe774fce51
KeyboardCombination <44244130+KeyboardCombination@users.noreply.github.com>
2020-09-05 12:43:43 -0400

    Update mobile.js

--- JS/mobile.js
+++ JS/mobile.js
@@ -1,7 +1,5 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    console.log("is mobile")
     document.getElementById("css").href = "CSS/mobile.css"
 }else{
-    console.log("is desktop")
     document.getElementById("css").href = "CSS/main.css"
 }
