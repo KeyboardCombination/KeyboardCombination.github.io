49ef9581d012a9481635774d364c51062aeef1d3
KeyboardCombination <gamerbear256@gmail.com>
2022-07-15 16:44:50 -0400

    overhaul of everything

--- JS/mobile.js
+++
@@ -1,5 +0,0 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    document.getElementById("css").href = "CSS/mobile.css"
-}else{
-    document.getElementById("css").href = "CSS/main.css"
-}
